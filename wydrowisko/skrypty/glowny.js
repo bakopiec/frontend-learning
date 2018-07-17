7980ea756c22448daef79895bb4f52ea5ad8ded0
Barbara Kopiec <bakopiec@gmail.com>
2018-07-17 21:03:53 +0200

    Dodanie manipulacji drzewem DOM

--- wydrowisko/skrypty/glowny.js
+++ wydrowisko/skrypty/glowny.js
@@ -1,3 +1,13 @@
 var SELEKTOR_DUZEGO_OBRAZU = '[data-typ-obrazu="cel"]';
 var SELEKTOR_TYTULU_OBRAZU = '[data-typ-obrazu="tytul"]';
 var SELEKTOR_MINIATURY = '[data-typ-obrazu="wyzwalacz"]';
+
+var selektor = document.querySelector(SELEKTOR_DUZEGO_OBRAZU);
+console.log(selektor);
+
+var src = selektor.src;
+console.log(src);
+
+//selektor.src = 'obrazy/wydra2.jpg';
+
+selektor.setAttribute('src', 'obrazy/wydra2.jpg');
