62abf0461bfca9a98184a815dd478f374f702eec
Barbara Kopiec <bakopiec@gmail.com>
2018-07-19 21:19:45 +0200

    Dodanie funkcji edytującej główny obraz i napis

--- wydrowisko/skrypty/glowny.js
+++ wydrowisko/skrypty/glowny.js
@@ -2,12 +2,10 @@ var SELEKTOR_DUZEGO_OBRAZU = '[data-typ-obrazu="cel"]';
 var SELEKTOR_TYTULU_OBRAZU = '[data-typ-obrazu="tytul"]';
 var SELEKTOR_MINIATURY = '[data-typ-obrazu="wyzwalacz"]';
 
-var selektor = document.querySelector(SELEKTOR_DUZEGO_OBRAZU);
-console.log(selektor);
-
-var src = selektor.src;
-console.log(src);
-
-//selektor.src = 'obrazy/wydra2.jpg';
-
-selektor.setAttribute('src', 'obrazy/wydra2.jpg');
+function zmienObraz(urlObrazu, tekstTytulu) {
+    'use strict';
+    var duzyObraz = document.querySelector(SELEKTOR_DUZEGO_OBRAZU);
+    duzyObraz.setAttribute('src', urlObrazu);
+    var tytulObrazu = document.querySelector(SELEKTOR_TYTULU_OBRAZU);
+    tytulObrazu.textContent = tekstTytulu;
+}
