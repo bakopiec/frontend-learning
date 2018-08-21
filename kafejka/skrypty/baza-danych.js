ea6720c1816174dc8ae336d2c5b5823cde5da612
Barbara Kopiec <bakopiec@gmail.com>
2018-08-21 20:36:28 +0200

    Dodanie nowych funkcji do modułu bazy danych

--- kafejka/skrypty/baza-danych.js
+++ kafejka/skrypty/baza-danych.js
@@ -3,10 +3,27 @@
 
     var aplikacja = window.aplikacja || {};
 
-    function bazaDanych() {
-        console.log('wywołanie funkcj bazaDanych');
+    function BazaDanych() {
+        this.dane = {};
     }
 
-    aplikacja.bazaDanych = bazaDanych;
+    BazaDanych.prototype.dodaj = function(klucz, wartosc) {
+        this.dane[klucz] = wartosc;
+    }
+
+    BazaDanych.prototype.pobierz = function(klucz) {
+        var wartosc = this.dane[klucz];
+        return wartosc;
+    }
+
+    BazaDanych.prototype.pobierzWszystko = function() {
+        return this.dane;
+    }
+    
+    BazaDanych.prototype.usun = function(klucz) {
+        delete this.dane[klucz];
+    }
+
+    aplikacja.BazaDanych = BazaDanych;
     window.aplikacja = aplikacja;
 })(window);
