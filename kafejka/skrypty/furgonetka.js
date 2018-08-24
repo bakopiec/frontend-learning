8fd10117e30e5a074a12910cf7a621137237e40e
Barbara Kopiec <bakopiec@gmail.com>
2018-08-24 19:47:59 +0200

    Dodanie obsługi zamówień

--- kafejka/skrypty/furgonetka.js
+++ kafejka/skrypty/furgonetka.js
@@ -8,6 +8,16 @@
         this.bazaDanych = bazaDanych;
     }
 
+    Furgonetka.prototype.zlozZamowienie = function(zamowinie) {
+        console.log('Złożenie zamówienia dla ' + zamowinie.email);
+        this.bazaDanych.dodaj(zamowinie.email, zamowinie);
+    }
+
+    Furgonetka.prototype.zrealizujZamowienie = function(idKlienta){
+        console.log('Zrealizowane zamówienie dla klienta ' + idKlienta);
+        this,this.bazaDanych.usun(idKlienta);
+    }
+
     aplikacja.Furgonetka = Furgonetka;
     window.aplikacja = aplikacja;
 })(window);
