7f4a441df7d5be999a08a401d14f98d0e402d186
Barbara Kopiec <bakopiec@gmail.com>
2018-08-25 21:40:02 +0200

    Ukończenie modułu furgonetka

--- kafejka/skrypty/furgonetka.js
+++ kafejka/skrypty/furgonetka.js
@@ -15,7 +15,15 @@
 
     Furgonetka.prototype.zrealizujZamowienie = function(idKlienta){
         console.log('Zrealizowane zamówienie dla klienta ' + idKlienta);
-        this,this.bazaDanych.usun(idKlienta);
+        this.bazaDanych.usun(idKlienta);
+    }
+
+    Furgonetka.prototype.drukujZamowienie = function() {
+        var tablicaKlientow = Object.keys(this.bazaDanych.pobierzWszystko());
+        console.log('Furgonetka nr ' + this.identyfikator + ' ma niezrealizowane zamówienia:');
+        tablicaKlientow.forEach(function(id) {
+            console.log(this.bazaDanych.pobierz(id));
+        }.bind(this));
     }
 
     aplikacja.Furgonetka = Furgonetka;
---
+++ kafejka/skrypty/glowny.js
@@ -0,0 +1,7 @@
+(function(window) {
+    'use strict';
+
+    var Aplikacja = window.aplikacja;
+    var BazaDanych = Aplikacja.BazaDanych;
+    var Furgonetka = Aplikacja.Furgonetka;
+})(window)
