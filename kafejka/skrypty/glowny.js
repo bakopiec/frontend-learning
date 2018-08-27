2b36d12ce8349a87ce7d1629e5f17eef3858f378
Barbara Kopiec <bakopiec@gmail.com>
2018-08-27 21:00:15 +0200

    Utworzenie instancji modułu furgonetka

--- kafejka/skrypty/glowny.js
+++ kafejka/skrypty/glowny.js
@@ -4,4 +4,6 @@
     var Aplikacja = window.aplikacja;
     var BazaDanych = Aplikacja.BazaDanych;
     var Furgonetka = Aplikacja.Furgonetka;
-})(window)
+    var mojaFurgonetka = new Furgonetka('ncc-1701', new BazaDanych());
+    window.mojaFurgonetka = mojaFurgonetka; 
+})(window);
