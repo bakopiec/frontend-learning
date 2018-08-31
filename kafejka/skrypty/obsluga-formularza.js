5ebd4100cd8e72c5f6e6688d99e1fe68dc1cb654
Barbara Kopiec <bakopiec@gmail.com>
2018-08-31 21:15:19 +0200

    Dodanie biblioteki jQuery oraz pobranie zawartości formularza

--- kafejka/skrypty/obsluga-formularza.js
+++ kafejka/skrypty/obsluga-formularza.js
@@ -2,8 +2,31 @@
     'use strict';
 
     var aplikacja = window.Aplikacja || {};
+    var $ = window.jQuery;
 
-    function ObslugaFormularza() {}
+    function ObslugaFormularza(selektor) {
+        if (!selektor) {
+            throw new Error('Brak selektora');
+        }
+        this.$elementFormularza = $(selektor);
+        if (this.$elementFormularza.length === 0) {
+            throw new Error('Brak elementów odpowiadających selektorowi: ' + selektor);
+        }
+    }
+    
+    ObslugaFormularza.prototype.dodajObslugeWysylki = function(fn) {
+        console.log('Utworzenie obsługi zdarzenia submit formularza');
+        this.$elementFormularza.on('submit', function(zdarzenie) {
+            zdarzenie.preventDefault();
+            var dane = {};
+            $(this).serializeArray().forEach(function(element) {
+                dane[element.name] = element.value;
+                console.log('Element ' + element.name + ' ma wartość ' + element.value);
+            });
+            console.log(dane);
+            fn(dane);
+        });
+    }
 
     aplikacja.ObslugaFormularza = ObslugaFormularza;
     window.Aplikacja = aplikacja;
