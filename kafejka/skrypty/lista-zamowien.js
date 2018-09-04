1d234bfe9dfe6cf770d1287775250c1a8952d13f
Barbara Kopiec <bakopiec@gmail.com>
2018-09-04 20:42:23 +0200

    Dodanie modułu wiersz

--- kafejka/skrypty/lista-zamowien.js
+++ kafejka/skrypty/lista-zamowien.js
@@ -14,6 +14,31 @@
         }
     }
 
+    function Wiersz(zamowienie) {
+        var $div = $('<div></div>', {
+            'data-zamowienie': 'pole wyboru',
+            'class': 'checkbox'
+        });
+        var $etykieta = $('<label></label>');
+        var $poleWyboru = $('<input></input>', {
+            type: 'checkbox',
+            value: zamowienie.adresEmail
+        });
+        var opis = zamowienie.wielkosc + ' ';
+        if (zamowienie.smak) {
+            opis += zamowienie.smak + ' ';
+        }
+        opis += zamowienie.kawa + ', ';
+        opis += '(' + zamowienie.adresEmail + ')';
+        opis += '[' + zamowienie.moc + 'x]';
+
+        $etykieta.append($poleWyboru);
+        $etykieta.append(opis);
+        $div.append($etykieta);
+
+        this.$element = $div;
+    }
+
     aplikacja.ListaZamowien = ListaZamowien;
     window.Aplikacja = aplikacja;
 })(window);
