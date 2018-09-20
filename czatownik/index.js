4da683f693ff837755a40128dae1e64789346056
Barbara Kopiec <bakopiec@gmail.com>
2018-09-20 21:00:51 +0200

    Utworzenie funkcjonalności pobierającej dane z serwera

--- czatownik/index.js
+++ czatownik/index.js
@@ -1,7 +1,18 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
 var server = http.createServer(function(zapytanie, odpowiedz) {
     console.log('Odpowiedź na zapytanie');
-    odpowiedz.end('<h1>Witaj, mój świecie</h1>');
+    var url = zapytanie.url;
+    var nazwaPliku = 'index.html';
+    if (url.length > 1) {
+        nazwaPliku = url.substring(1);
+    }
+    console.log(nazwaPliku);
+    var sciezkaPliku = path.resolve(__dirname, 'aplikacja', nazwaPliku);
+    fs.readFile(sciezkaPliku, function(blad, dane) {
+        odpowiedz.end(dane);
+    });
 });
 server.listen(3000);
