bc494be6c80c7968ddccf9b6635f5ed1ae9bb180
Barbara Kopiec <bakopiec@gmail.com>
2018-10-11 20:48:25 +0200

    Dodanie nowych widoków oraz odnośnika do serwera

--- tropiciel/app/models/swiadek.js
+++ tropiciel/app/models/swiadek.js
@@ -4,5 +4,8 @@ export default DS.Model.extend({
     imie: DS.attr('string'),
     nazwisko: DS.attr('string'),
     email: DS.attr('string'),
-    obserwacje: DS.hasMany('obserwacja')
+    obserwacje: DS.hasMany('obserwacja'),
+    imieNazwisko: Ember.computed('imie', 'nazwisko', function() {
+        return this.get('imie') + ' ' + this.get('nazwisko');
+    })
 });
